4ea52fa3722410b6fe9b212aee22d2eb3ace7a0b
Hendrik Weideman <weideh@rpi.edu>
2016-01-28 10:53:05 -0500

    Added links to original code for future reference.

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# For Ubuntu 14.04+
+# https://faq.i3wm.org/question/239/how-do-i-suspendlockscreen-and-logout/?answer=5557#post-id-5557
 
 dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 \
       "org.freedesktop.login1.Manager.PowerOff" boolean:true
